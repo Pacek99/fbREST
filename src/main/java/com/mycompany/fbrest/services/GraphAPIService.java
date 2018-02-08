a69a6ef4a13a71d449f6629656192fd6f13e54b9
Patrik Rojek <pacek99@gmail.com>
2018-02-08 06:57:17 +0100

    upravy na reste na stahovanie eventov .... uz tam len bude treba doplnit dalsi parameter na objekt listanera a volanie metody hotovo na konci stahovania

--- src/main/java/com/mycompany/fbrest/services/GraphAPIService.java
+++ src/main/java/com/mycompany/fbrest/services/GraphAPIService.java
@@ -5,7 +5,6 @@
  */
 package com.mycompany.fbrest.services;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import org.apache.http.HttpResponse;
 import java.io.IOException;
 import java.util.List;
@@ -14,15 +13,11 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mycompany.fbrest.models.EventDTO;
-import com.mycompany.fbrest.models.EventElastic;
 import com.mycompany.fbrest.models.ParserJSON;
 import events.Launcher;
 import events.entities.Event;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Iterator;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -31,34 +26,43 @@ import org.json.JSONObject;
  * @author Patrik Rojek
  */
 public class GraphAPIService {
-    
-    
-    public void saveEvents(String source) throws JSONException{
+
+    public void saveEvents(String source) throws JSONException {
         List<Event> allRecievedEvents = GraphAPIService.getEventsFromSource(source);
         List<Event> newRecievedEvents = new ArrayList<>();
         for (Event recievedEvent : allRecievedEvents) {
-            if (Launcher.eventService.existsById(recievedEvent.id)) {
+            if (!Launcher.eventService.existsById(recievedEvent.id)) {
                 newRecievedEvents.add(recievedEvent);
             }
         }
         //3. Vytiahneš všetky staré z databázy (najskôr všetky, tu si nie som istý).
         Iterable<Event> eventsFromDB = Launcher.eventService.findAll();
-        //4. Vytvoríš dvojice nových a starých + dvojice nových navzájom.
-        Map<String, String> mapaIdcok = new HashMap<>();
-        
-        //5. Uložíš všetky dvojice dvojice ako EventsSimilarities. 
-        for (Map.Entry<String, String> entry : mapaIdcok.entrySet()) {
-            //Launcher.eventsSimilarityService.saveNew(entry.getKey(), entry.getValue(), Launcher.similarityCalculator.);
-            
+
+        //4. Uložíš ešte aj nové eventy...
+        Launcher.eventService.saveAll(newRecievedEvents);
+
+        //5. Vytvoríš dvojice nových a starých + dvojice nových navzájom. 
+        // + aj 6. Uložíš všetky dvojice dvojice ako EventsSimilarities.
+        for (Event newE : newRecievedEvents) {
+            for (Event e : eventsFromDB) {
+                Launcher.eventsSimilarityService.saveNew(e.id, newE.id, Launcher.similarityCalculator.calculateSimilarityCoefficient(e, newE));
+            }
         }
-        //6. Uložíš ešte aj nové eventy...
-        
+
+        // tu este treba dvojice novych navzajom
+        for (int i = 0; i < newRecievedEvents.size(); ++i) {
+            for (int j = i + 1; j < newRecievedEvents.size(); ++j) {
+                Launcher.eventsSimilarityService.saveNew(newRecievedEvents.get(i).id, newRecievedEvents.get(j).id, Launcher.similarityCalculator.calculateSimilarityCoefficient(newRecievedEvents.get(i), newRecievedEvents.get(j)));
+            }
+        }
+
+        //7. zavolas metodu hotovo 
     }
-    
-    public static List<Event> getEventsFromSource(String source) throws JSONException{
+
+    public static List<Event> getEventsFromSource(String source) throws JSONException {
         HttpClient client = HttpClientBuilder.create().build();
         HttpGet request = new HttpGet("https://graph.facebook.com/v2.11/" + source + "/events?access_token=EAACEdEose0cBAHGDfinXKDweEqYCZBTdxhqGuYSazrRKjdgxa1HZBE8A4c5RMZBCzBTGHIWIZCA8Ao7ET77nqqXyl7hOQWsbVsA7sVhD6BQZC0FzN4MBRf1z0WThEdz2et1AEZBZBTTQtXbJi6sJjBQAEHrngkAS1BiuWn7gdiGK2SigbNIbsvZB6RbNMtbIoUZAtwY3gZCtWCeAZDZD");
-        
+
         List<Event> data = new ArrayList<>();
         try {
             HttpResponse response = client.execute(request);
@@ -66,17 +70,17 @@ public class GraphAPIService {
 
             // Read the contents of an entity and return it as a String.
             String content = EntityUtils.toString(entity);
-            
+
             data = ParserJSON.parsujJSON(content, source);
-            
+
             //System.out.println(data.size());            
         } catch (IOException e) {
             e.printStackTrace();
-        }    
-        return data; 
+        }
+        return data;
     }
-    
-    public static List<Event> getEventsFromURL(String url, String source) throws JSONException{
+
+    public static List<Event> getEventsFromURL(String url, String source) throws JSONException {
         HttpClient client = HttpClientBuilder.create().build();
         HttpGet request = new HttpGet(url);
 
@@ -87,16 +91,16 @@ public class GraphAPIService {
 
             // Read the contents of an entity and return it as a String.
             String content = EntityUtils.toString(entity);
-            
+
             data = ParserJSON.parsujJSON(content, source);
-            
+
             //System.out.println(data.size());            
         } catch (IOException e) {
             e.printStackTrace();
-        }    
-        return data; 
+        }
+        return data;
     }
-    
+
     public static void main(String[] args) throws JSONException {
         /*
         HttpClient client = HttpClientBuilder.create().build();
@@ -122,8 +126,8 @@ public class GraphAPIService {
             System.out.println(dalsia);            
         } catch (IOException e) {
             e.printStackTrace();
-        }*/   
+        }*/
         System.out.println(GraphAPIService.getEventsFromSource("331514527167").size());
     }
-    
+
 }
