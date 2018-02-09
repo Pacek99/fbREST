7dd173035e9fa4f91759eed47c2e2f6ec2c379c2
Patrik Rojek <pacek99@gmail.com>
2018-02-09 16:28:55 +0100

    pridanie pomocneho resourcu iba na kontrolu či su nejake eventy pridane do prazdnej DB po spusteni resourcu na stiahnutie eventov

---
+++ src/main/java/com/mycompany/fbrest/resources/PomocnyResource.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.fbrest.resources;
+
+import events.Launcher;
+import events.entities.Event;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author Patrik Rojek
+ */
+
+/*tento resource je iba pomocný resource na zistenie či su nejake date v elastic DB, vypis*/
+@Path("/control")
+public class PomocnyResource {
+    
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Iterable<Event> getEvents(){
+        return Launcher.eventService.findAll();
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public long numberOfEventsInDB(){
+        return Launcher.eventService.findAll().spliterator().getExactSizeIfKnown();
+    }
+    
+}
