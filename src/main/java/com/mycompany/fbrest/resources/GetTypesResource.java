910cdf92bc147b4be7a8dab9ac2fd303e859f477
Patrik Rojek <pacek99@gmail.com>
2018-02-10 18:33:54 +0100

    rest na vratenie moznych typov

---
+++ src/main/java/com/mycompany/fbrest/resources/GetTypesResource.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.fbrest.resources;
+
+import eventagent.persistence.entities.EventDefaultType;
+import eventagent.persistence.entities.SourceType;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author Patrik Rojek
+ */
+
+@Path("/getTypes")
+public class GetTypesResource {
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Object[] isAdmin(){
+        SourceType[] aaa = SourceType.values();
+        EventDefaultType[] bbb = EventDefaultType.values();
+        Object[] o = new Object[2];
+        o[0] = aaa;
+        o[1] = bbb;
+        return o;
+    }
+}
