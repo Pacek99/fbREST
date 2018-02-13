d7493c137d1cadea8b46bd6a3e573f93207e9476
Patrik Rojek <pacek99@gmail.com>
2018-02-13 02:42:45 +0100

    skusanie

--- src/main/java/com/mycompany/fbrest/StartupListener.java
+++ src/main/java/com/mycompany/fbrest/StartupListener.java
@@ -6,27 +6,21 @@
 package com.mycompany.fbrest;
 
 import events.Launcher;
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
 
 /**
  *
  * @author Patrik Rojek
  */
-/*
-@WebListener
-public class StartupListener implements ServletContextListener {
+@Startup
+@Singleton
+public class StartupListener {
 
-    @Override
-    public void contextInitialized(ServletContextEvent event) {
+    @PostConstruct
+    public void init() {
         // Perform action during application's startup
         Launcher.main(null);
     }
-
-    @Override
-    public void contextDestroyed(ServletContextEvent sce) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 }
-*/
